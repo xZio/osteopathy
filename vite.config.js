129ad1dfbfc8d94600b558284d51db48c6ec46b9
Alexander Botanov <sky_11@inbox.ru>
2025-10-08 13:21:04 +0300

    2nd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // For GitHub Pages: repository is xZio/osteopathy -> base should be "/osteopathy/"
+  base: '/osteopathy/',
   plugins: [
     react({
       babel: {
