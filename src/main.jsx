a0ec7e4d97bea3df6081a2c800700eaa59aeca8c
Alexander Botanov <sky_11@inbox.ru>
2025-10-31 10:04:50 +0300

    Remove dynamic basename from BrowserRouter in main.jsx for simplified routing configuration

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,9 @@ import "./vendor/fonts.css";
 import App from "../src/components/App/App";
 import { BrowserRouter } from "react-router-dom";
 
-const basename = import.meta.env.VITE_BASENAME || '';
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
